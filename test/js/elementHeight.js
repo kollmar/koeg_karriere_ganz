e54ddafda78a2763b6535463c748acf8de9d1d60
AK <a.kollmar@koenigsteiner.de>
2016-05-11 10:31:05 +0200

    elementHeight.js nun mit Kommentaren

--- test/js/elementHeight.js
+++ test/js/elementHeight.js
@@ -1,3 +1,12 @@
+/*
+*
+*	@Thema:	Schleife für die Bereiche, die horizontal Bündig gemacht werden.
+*			Verwendet wurde eine Klasse (Content), die in den Menüpunkten (Vertrieb, 
+*		  	Personalmarketing, Kundenberatung, Mediengestalter, Azubi_studenten)
+*		  	hinterlegt wurden.    
+* 
+* 
+*/
 var maxLenght = 0;
 var content = document.getElementsByClassName('content');
 var azubi_content = document.getElementsByClassName('azubi_contentHeight');
@@ -5,20 +14,25 @@ var testheight = content[1].offsetHeight +'px';
 	content[0].style.height = testheight;
 
 for (var i = 0; i < content.length; i++) {	
-	if (i === 2){
-		var leftContentHeight = {
-			pos: i,
-			height: content[i].offsetHeight
+	if (i === 2){										// linker Content(Klasse - 1 Textabsatz, der Bündig mit dem rechten Content sein soll)
+		var leftContentHeight = {						// Objekt
+			pos: i,										// - Wievielter Content
+			height: content[i].offsetHeight				// - Die Höhe des Contents
 		} 
 	}
-	else if (i === 4){
+	else if (i === 4){									// rechtes Content (Klasse - Zitat & Bild)
 		var rightContentHeight = {
 			pos: i,
 			height: content[i].offsetHeight
 		}
 	}
-	else if (azubi_content.length > 0){
-		if (i === 5){
+	/*
+	*	Explizit musste der Azubi_Studenten-Bereich hinterlegt werden,
+	*	da dieser eine andere Content-Anzahl besitzt und der Content rechts bündig mit einem h2-tag sein sollte statt mit einem ganzen Content
+	* 
+	*/
+	else if (azubi_content.length > 0){					// wird geprüft, ob es denn auf der Seite Klasse 'azubi_contentHeight' existiert
+		if (i === 5){									// 6. Content
 			var rightContentHeight = {
 				pos: i,
 				height: content[i].offsetHeight
@@ -44,7 +58,15 @@ else {
 	console.log('bar');
 	setContentHeight(leftContentHeight.pos, rightContentHeight, false, 'right');
 }
-
+/*
+*	
+*	@function (	
+*	          	[Position des Content dessen Höhe angepasst wird], 
+*				[die Höhe die benötigt wird], [soll der Margin-Bottom Abstand des Klasse bleiben?], 
+*				[welcher Abstand - linker oder rechter Content - soll entfernt werden]
+*			  ) 
+*			  
+*/
 function setContentHeight (pos, contentHeight, margBottom, margContent){
 	
 	if (azubi_content.length > 0 ){
