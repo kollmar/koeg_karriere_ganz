5aaf3f596e4f69606f5620d65f7172e5a5a1b272
AK <a.kollmar@koenigsteiner.de>
2016-05-11 16:23:25 +0200

    Kontakt-Bereich angepasst - elementHeight aktualisiert; Prüft nun zuvor die Browserbreite

--- test/js/elementHeight.js
+++ test/js/elementHeight.js
@@ -1,63 +1,76 @@
 /*
-*
-*	@Thema:	Schleife für die Bereiche, die horizontal Bündig gemacht werden.
-*			Verwendet wurde eine Klasse (Content), die in den Menüpunkten (Vertrieb, 
-*		  	Personalmarketing, Kundenberatung, Mediengestalter, Azubi_studenten)
-*		  	hinterlegt wurden.    
+*	@Author: Andre Kollmar - Königsteiner Agentur Karlsruhe
+*	@Thema:	 Schleife für die Bereiche, die horizontal Bündig gemacht werden.
+*			 Verwendet wurde eine Klasse (Content), die in den Menüpunkten (Vertrieb, 
+*		  	 Personalmarketing, Kundenberatung, Mediengestalter, Azubi_studenten)
+*		  	 hinterlegt wurden.    
 * 
 * 
 */
-var maxLenght = 0;
-var content = document.getElementsByClassName('content');
+var maxLenght 	  = 0;
+var i = 0;
+var content 	  = document.getElementsByClassName('content');
 var azubi_content = document.getElementsByClassName('azubi_contentHeight');
-var testheight = content[1].offsetHeight +'px';
-	content[0].style.height = testheight;
+var werte_content = document.getElementsByClassName('content_werte');
+var screenWidth	  = window.innerWidth;
 
-for (var i = 0; i < content.length; i++) {	
-	if (i === 2){										// linker Content(Klasse - 1 Textabsatz, der Bündig mit dem rechten Content sein soll)
-		var leftContentHeight = {						// Objekt
-			pos: i,										// - Wievielter Content
-			height: content[i].offsetHeight				// - Die Höhe des Contents
-		} 
-	}
-	else if (i === 4){									// rechtes Content (Klasse - Zitat & Bild)
-		var rightContentHeight = {
-			pos: i,
-			height: content[i].offsetHeight
-		}
-	}
-	/*
-	*	Explizit musste der Azubi_Studenten-Bereich hinterlegt werden,
-	*	da dieser eine andere Content-Anzahl besitzt und der Content rechts bündig mit einem h2-tag sein sollte statt mit einem ganzen Content
-	* 
-	*/
-	else if (azubi_content.length > 0){					// wird geprüft, ob es denn auf der Seite Klasse 'azubi_contentHeight' existiert
-		if (i === 5){									// 6. Content
-			var rightContentHeight = {
-				pos: i,
-				height: content[i].offsetHeight
+if (screenWidth > 620){											// Prüft die Bildschirmbreite und führt erst das Script 
+
+	if (content.length > 0){
+
+		var testheight = content[1].offsetHeight +'px';
+			content[0].style.height = testheight;
+
+		for (var i = 0; i < content.length; i++) {	
+			if (i === 2){										// linker Content(Klasse - 1 Textabsatz, der Bündig mit dem rechten Content sein soll)
+				var leftContentHeight = {						// Objekt
+					pos: i,										// - Wievielter Content
+					height: content[i].offsetHeight				// - Die Höhe des Contents
+				} 
+			}
+			else if (i === 4){									// rechtes Content (Klasse - Zitat & Bild)
+				var rightContentHeight = {
+					pos: i,
+					height: content[i].offsetHeight
+				}
+			}
+			/*
+			*	Explizit musste der Azubi_Studenten-Bereich hinterlegt werden,
+			*	da dieser eine andere Content-Anzahl besitzt und der Content rechts bündig mit einem h2-tag sein sollte statt mit einem ganzen Content
+			* 
+			*/
+			else if (azubi_content.length > 0){					// wird geprüft, ob es denn auf der Seite Klasse 'azubi_contentHeight' existiert
+				if (i === 5){									// 6. Content
+					var rightContentHeight = {
+						pos: i,
+						height: content[i].offsetHeight
+					}
+				}
 			}
 		}
+
+		if (azubi_content.length > 0){	
+
+			for (i = 0; i < azubi_content.length; i++) {
+				maxLenght += azubi_content[i].offsetHeight;
+			}
+			setContentHeight(rightContentHeight.pos, maxLenght, true, 'left');
+		}
+		else if (leftContentHeight.height > rightContentHeight.height){
+			setContentHeight(rightContentHeight.pos, leftContentHeight, false, 'left');
+		}
+		else {
+			setContentHeight(leftContentHeight.pos, rightContentHeight, false, 'right');
+		}
+
+	}
+
+	if (werte_content.length > 0){
+		werte_content[2].style.height = werte_content[0].offsetHeight + 'px';
+		werte_content[2].style.marginBottom = '23px';
 	}
 }
 
-if (azubi_content.length > 0){	
-
-	for (var i = 0; i < azubi_content.length; i++) {
-		maxLenght += azubi_content[i].offsetHeight;
-	}
-	console.log('left: '+maxLenght+ ', '+ rightContentHeight.height);
-	setContentHeight(rightContentHeight.pos, maxLenght, true, 'left');
-}
-else if (leftContentHeight.height > rightContentHeight.height){
-	console.log('foo');
-	setContentHeight(rightContentHeight.pos, leftContentHeight, false, 'left');
-}
-else {
-
-	console.log('bar');
-	setContentHeight(leftContentHeight.pos, rightContentHeight, false, 'right');
-}
 /*
 *	
 *	@function (	
